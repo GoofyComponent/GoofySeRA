356e1fadaa98739d254e9c958f817aea8fa1ccab
Dest.Com <antoine.azevedo-da-silva@hetic.net>
2023-06-13 00:35:41 +0200

    Working docker compose for front end

---
+++ sera-front/app/test/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div>
+      <a href="https://portfolio.destcom.website">test</a>
+      <p>
+        AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+      </p>
+    </div>
+  );
+}
