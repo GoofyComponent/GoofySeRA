3cab7d7f980ce9d985e11ed9f23027d3f5495702
Antoine Azevedo Da Silva <azevedo-a@hotmail.fr>
2023-06-19 15:21:13 +0200

    Add shadcn library

--- sera-front/app/test/page.tsx
+++ sera-front/app/test/page.tsx
@@ -1,7 +1,19 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
 export default function Home() {
   return (
-    <>
-      <p className="text-sera-periwinkle">Hello ^^ !</p>
-    </>
+    <Accordion type="single" collapsible>
+      <AccordionItem value="item-1">
+        <AccordionTrigger>
+          Est-ce que la librairie de composant est installée ?
+        </AccordionTrigger>
+        <AccordionContent>Oui j'en ai bien l'impression !</AccordionContent>
+      </AccordionItem>
+    </Accordion>
   );
 }
