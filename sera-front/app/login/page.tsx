4285491aa0c9746aa5ca6fb315c65a9de3378989
Dest.Com <antoine.azevedo-da-silva@hetic.net>
2023-06-20 00:36:13 +0200

    Add login route
    - Added favicon
    - Added form UI component
    - Added other UI components
    - Added login form
    - Delete useless NextJs startup datas

---
+++ sera-front/app/login/page.tsx
@@ -0,0 +1,13 @@
+import { LoginForm } from "@/components/LoginForm";
+export default function Home() {
+  return (
+    <main className="mt-auto flex h-[80vh] flex-col items-center justify-center">
+      <section className="m-auto flex w-10/12 flex-col items-center justify-center rounded-xl border-2 border-sera-periwinkle p-6 text-sera-periwinkle xl:w-4/12">
+        <h1 className="my-12 text-3xl font-semibold xl:text-5xl">
+          Welcome back !
+        </h1>
+        <LoginForm />
+      </section>
+    </main>
+  );
+}
