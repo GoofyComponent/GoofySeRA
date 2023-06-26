a316ddbc4dde7f6018befe3068d0ff4d43909ad2
Dest.Com <antoine.azevedo-da-silva@hetic.net>
2023-06-26 20:28:01 +0200

    Updated images errors and redirection to decorative routes

--- sera-front/next.config.js
+++ sera-front/next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/login",
+        permanent: true,
+      },
+      {
+        source: "/dashboard",
+        destination: "/login",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig;
