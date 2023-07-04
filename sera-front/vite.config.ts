5f02c0cc0d2802dbd5d5d73e3587d7dddf1e39a0
Antoine Azevedo Da Silva <azevedo-a@hotmail.fr>
2023-07-04 14:07:07 +0200

    Add Docker for front

--- sera-front/vite.config.ts
+++ sera-front/vite.config.ts
@@ -4,6 +4,9 @@ import vitePluginFaviconsInject from "vite-plugin-favicons-inject";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  server: {
+    port: 3000,
+  },
   plugins: [
     react(),
     vitePluginFaviconsInject("./src/assets/images/favicon.svg"),
