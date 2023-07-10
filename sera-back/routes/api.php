7fdc9b5d7e502eb6e217c34f0046133f193acba8
Adrien Albuquerque <adrialb95@gmail.com>
2023-07-10 15:15:14 +0200

    Update api.php

--- sera-back/routes/api.php
+++ sera-back/routes/api.php
@@ -14,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::middleware(['auth:sanctum'])->get('me', 'App\Http\Controllers\UserController@getAuthenticatedUser')->name('me');
-
-Route::get('me', 'App\Http\Controllers\UserController@getAuthenticatedUser')->name('me');
+Route::middleware(['auth:sanctum'])->get('me', 'App\Http\Controllers\UserController@getAuthenticatedUser')->name('me');
 
 Route::group(['middleware' => ['App\Http\Middleware\CheckRoleAccess']], function () {
 
