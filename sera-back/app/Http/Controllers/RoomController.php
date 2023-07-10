d7f1c1d30cb91fc0c542b2ab910251f6d5a8d5f6
Adrien Albuquerque <adrialb95@gmail.com>
2023-07-10 11:53:32 +0200

    Room Migration + Room Ressource

---
+++ sera-back/app/Http/Controllers/RoomController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Room;
+use Illuminate\Http\Request;
+
+class RoomController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $rooms = Room::all();
+
+        if ($rooms === null) {
+            return response()->json(['error' => 'No rooms found.'], 400);
+        }
+
+        return $rooms;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string',
+        ]);
+
+        $room = new Room();
+        $room->name = $validated['name'];
+        $room->save();
+
+        return $room;
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($id)
+    {
+        $room = Room::find($id);
+
+        if ($room === null) {
+            return response()->json(['error' => 'Room not found.'], 400);
+        }
+
+        return $room;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, $id)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string',
+        ]);
+
+        $room = Room::find($id);
+
+        if ($room === null) {
+            return response()->json(['error' => 'Room not found.'], 400);
+        }
+
+        $room->name = $validated['name'];
+        $room->save();
+
+        return $room;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($id)
+    {
+        $room = Room::find($id);
+
+        if ($room === null) {
+            return response()->json(['error' => 'Room not found.'], 400);
+        }
+
+        $room->delete();
+
+        return response()->json(['success' => 'Room deleted.'], 200);
+    }
+}
