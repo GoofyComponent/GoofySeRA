24b5e1d1871ece49452e15df7fd0703812c23233
Dest.Com <37459465+DestroyCom@users.noreply.github.com>
2023-07-10 18:41:42 +0200

    Update cors.php

--- sera-back/config/cors.php
+++ sera-back/config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
     
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['register', 'login', 'logout,'api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
     'allowed_origins' => [env('FRONTEND_URL', 'http://127.0.0.1:3000', 'http://127.0.0.1')],
     'allowed_origins_patterns' => [],
