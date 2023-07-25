e68a8e8955460e0adf2c79b5c7892fe738311968
Adrien Albuquerque <adrialb95@gmail.com>
2023-07-25 23:53:58 +0200

    Fix response Project request + more Project and Project request generated by the seeder

--- sera-back/database/seeders/ProjectSeeder.php
+++ sera-back/database/seeders/ProjectSeeder.php
@@ -12,6 +12,6 @@ class ProjectSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\Project::factory()->count(5)->create();
+        \App\Models\Project::factory()->count(15)->create();
     }
 }
