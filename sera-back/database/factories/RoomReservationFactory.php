a594bca917a00875254933bfeef88c3392e3d40c
ThomAzgo <thomas.buisson@hetic.net>
2023-08-09 12:21:44 +0200

    moi quand salle à reserver

--- sera-back/database/factories/RoomReservationFactory.php
+++ sera-back/database/factories/RoomReservationFactory.php
@@ -1,7 +1,32 @@
 <?php
 
-namespace Database\Seeders;
+namespace Database\Factories;
 
-use App\Models\Team;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
+use App\Models\ProjectRequest;
+use Illuminate\Support\Carbon;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Project>
+ */
+class RoomReservationFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition (): array
+    {
+        return [
+            'room_id' => $this->faker->numberBetween(1, 10),
+            'project_id' => $this->faker->numberBetween(1, 10),
+            'date' => $this->faker->dateTimeBetween('now', '+1 years'),
+            'start_time' => $this->faker->dateTimeBetween('now', '+1 years'),
+            'end_time' => $this->faker->dateTimeBetween('now', '+1 years'),
+            'title' => $this->faker->sentence(3),
+            'created_at' => $this->faker->dateTimeBetween('now', '+1 years'),
+            'updated_at' => $this->faker->dateTimeBetween('now', '+1 years'),
+        ];
+    }
+}
