46b25cd3a75b77cfbc15cf8f474b4cf2a92d34dc
Adrien Albuquerque <adrialb95@gmail.com>
2023-08-10 11:55:05 +0100

    Update RoomReservationFactory.php

--- sera-back/database/factories/RoomReservationFactory.php
+++ sera-back/database/factories/RoomReservationFactory.php
@@ -16,17 +16,32 @@ class RoomReservationFactory extends Factory
      *
      * @return array<string, mixed>
      */
-    public function definition (): array
+    public function definition(): array
     {
+        $rooms = \App\Models\Room::all()->pluck('id')->toArray();
+        if (empty($rooms)) {
+            \App\Models\Room::factory()->count(10)->create();
+        }
+
+        $projects = \App\Models\Project::all()->pluck('id')->toArray();
+        if (empty($projects)) {
+            \App\Models\Project::factory()->count(10)->create();
+        }
+
+        $date = $this->faker->dateTimeBetween('now', '+1 years');
+        // Start time is between 8:00 and 18:00
+        $start_time = Carbon::instance($date)->setTime($this->faker->numberBetween(8, 18), 0, 0);
+        // End time is between 1 and 4 hours after start time
+        $end_time = Carbon::instance($start_time)->addHours($this->faker->numberBetween(1, 4));
+
         return [
-            'room_id' => $this->faker->numberBetween(1, 10),
-            'project_id' => $this->faker->numberBetween(1, 10),
-            'date' => $this->faker->dateTimeBetween('now', '+1 years'),
-            'start_time' => $this->faker->dateTimeBetween('now', '+1 years'),
-            'end_time' => $this->faker->dateTimeBetween('now', '+1 years'),
+            'room_id' => $this->faker->randomElement($rooms),
+            'project_id' => $this->faker->randomElement($projects),
+            'date' => $date,
+            'start_time' => $start_time->format('H:i'),
+            'end_time' => $end_time->format('H:i'),
             'title' => $this->faker->sentence(3),
-            'created_at' => $this->faker->dateTimeBetween('now', '+1 years'),
-            'updated_at' => $this->faker->dateTimeBetween('now', '+1 years'),
         ];
     }
+
 }
