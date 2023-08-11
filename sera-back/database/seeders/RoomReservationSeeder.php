d08353ff8b1981f6c8b55afc6ef08298760c8c4d
ThomAzgo <thomas.buisson@hetic.net>
2023-08-12 01:10:50 +0200

    Update RoomReservationSeeder.php

--- sera-back/database/seeders/RoomReservationSeeder.php
+++ sera-back/database/seeders/RoomReservationSeeder.php
@@ -2,9 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\User;
+use Carbon\Carbon;
 
 class RoomReservationSeeder extends Seeder
 {
@@ -13,6 +12,25 @@ class RoomReservationSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\RoomReservation::factory()->count(10)->create();
+        $reservations = \App\Models\RoomReservation::factory()->count(10)->make();
+
+        foreach ($reservations as $reservation) {
+            do {
+                if ($reservation->id != 1){
+                    $lastReservation = \App\Models\RoomReservation::find($reservation->id - 1);
+                }
+                $overlap = true;
+                // if the time of the reservation is overlapping with the last reservation, create a new one
+                if (isset($lastReservation) && $lastReservation->room_id == $reservation->room_id && $lastReservation->date == $reservation->date) {
+                    $start_time = Carbon::instance($lastReservation->start_time)->addHour(1);
+                    $end_time = Carbon::instance($start_time)->addHours(rand(1, 4));
+                    $reservation->start_time = $start_time->format('H:i');
+                    $reservation->end_time = $end_time->format('H:i');
+                } else {
+                    $overlap = false;
+                }
+            } while ($overlap == true || $reservation->id == 1);
+            $reservation->save();
+        }
     }
 }
