05b8af660f252d099969f315f0c32bab2f1b1dae
ThomAzgo <thomas.buisson@hetic.net>
2023-08-18 01:09:01 +0200

    Update RoomReservationSeeder.php

--- sera-back/database/seeders/RoomReservationSeeder.php
+++ sera-back/database/seeders/RoomReservationSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Carbon\Carbon;
+use Faker\Factory as FakerFactory;
 
 class RoomReservationSeeder extends Seeder
 {
@@ -13,23 +14,22 @@ class RoomReservationSeeder extends Seeder
     public function run(): void
     {
         $reservations = \App\Models\RoomReservation::factory()->count(10)->make();
+        $faker = FakerFactory::create();
+        $date = $faker->dateTimeBetween('now', '+1 years');
 
         foreach ($reservations as $reservation) {
             do {
-                if ($reservation->id != 1){
-                    $lastReservation = \App\Models\RoomReservation::find($reservation->id - 1);
-                }
-                $overlap = true;
-                // if the time of the reservation is overlapping with the last reservation, create a new one
-                if (isset($lastReservation) && $lastReservation->room_id == $reservation->room_id && $lastReservation->date == $reservation->date) {
-                    $start_time = Carbon::instance($lastReservation->start_time)->addHour(1);
-                    $end_time = Carbon::instance($start_time)->addHours(rand(1, 4));
-                    $reservation->start_time = $start_time->format('H:i');
-                    $reservation->end_time = $end_time->format('H:i');
-                } else {
-                    $overlap = false;
-                }
-            } while ($overlap == true || $reservation->id == 1);
+                $reservation->date = $date;
+                $reservation->start_time = Carbon::instance($date)->setTime($faker->numberBetween(8, 18), 0, 0);
+                $reservation->end_time = Carbon::instance($reservation->start_time)->addHours(rand(1, 4));
+            } while (\App\Models\RoomReservation::where('room_id', $reservation->room_id)
+                ->where('date', $reservation->date)
+                ->where('start_time', '<=', $reservation->start_time)
+                ->where('end_time', '>=', $reservation->end_time)
+                ->exists());
+        }
+
+        foreach ($reservations as $reservation) {
             $reservation->save();
         }
     }
