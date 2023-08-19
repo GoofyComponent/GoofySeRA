4d03c89041d7e6aec750d15c9f2a9c82c299ac41
Adrien Albuquerque <adrialb95@gmail.com>
2023-08-20 01:22:07 +0200

    SharedRessource Controller + Iadd all route + index Example

---
+++ sera-back/app/Http/Controllers/SharedRessourceController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Ressource;
+use App\Models\Project;
+
+class SharedRessourceController extends Controller
+{
+
+    public function index(Request $request)
+    {
+        $this->validate($request, [
+            'project_id' => 'required|integer'
+        ]);
+
+        $project = Project::find($request->project_id);
+
+        if (!$project) {
+            throw new \Exception("Le projet n'existe pas");
+        }
+
+        $ressources = $project->getSharedRessources();
+
+        return response()->json($ressources);
+
+    }
+
+    public function store(Request $request)
+    {
+
+    }
+
+    public function show($id)
+    {
+
+    }
+
+    public function update(Request $request, $id)
+    {
+
+    }
+
+    public function destroy($id): void
+    {
+
+    }
+}
