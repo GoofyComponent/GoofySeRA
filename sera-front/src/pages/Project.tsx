e57308df53aabac7efaef92067bcc16c26b31a62
Antoine Azevedo Da Silva <azevedo-a@hotmail.fr>
2023-08-22 11:47:15 +0200

    Add Tabs to project page & add members section

--- sera-front/src/pages/Project.tsx
+++ sera-front/src/pages/Project.tsx
@@ -1,28 +1,19 @@
 import { useQuery } from "@tanstack/react-query";
-import { BadgeHelp } from "lucide-react";
 import { useParams } from "react-router-dom";
 
 import { HeaderTitle } from "@/components/app/navigation/HeaderTitle";
-import { Button } from "@/components/ui/button";
-import {
-  Dialog,
-  DialogContent,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
+import { MembersContainer } from "@/components/app/project/Members/MembersContainer";
+import { SharedContainer } from "@/components/app/project/SharedRessources/SharedContainer";
+import { StepsIndicatorContainer } from "@/components/app/project/StepsIndicator/StepsIndicatorContainer";
+import { Separator } from "@/components/ui/separator";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { axios } from "@/lib/axios";
 
 import { BigLoader } from "./skeletons/BigLoader";
-import { Label } from "@radix-ui/react-label";
-import { Input } from "@/components/ui/input";
-
-// import { SharedRessources } from "../components/ui/sharedRessources";
 
 export const Project = () => {
-  const params = useParams();
-  const id = params.ProjectId;
+  const { ProjectId: id } = useParams<{ ProjectId: string }>();
+
   const {
     data: projectData,
     isLoading,
@@ -36,109 +27,63 @@ export const Project = () => {
     },
   });
 
-  if (error) return <>{error} . Erreur </>;
+  if (error) return <> Erreur </>;
+
+  if (isLoading)
+    return (
+      <BigLoader loaderSize={42} bgColor="transparent" textColor="sera-jet" />
+    );
+
+  if (!projectData) return <>No project found</>;
 
   return (
     <>
-      <div>
-        {!isLoading ? (
-          <>
-            <HeaderTitle
-              title={projectData.title && projectData.title}
-              previousTitle="Projet"
-            />
-            <div className="mx-6 mt-10">
-              <h3 className="text-4xl">Description :</h3>
-              <p className="mt-2 text-xl">{projectData.description}</p>
+      <HeaderTitle
+        title={projectData.title && projectData.title}
+        previousTitle="Projet"
+      />
+      <div className="flex justify-between">
+        <Tabs defaultValue="resume" className="ml-6 w-8/12">
+          <TabsList className="bg-transparent p-0">
+            <TabsTrigger
+              value="resume"
+              className="rounded-b-none rounded-t-sm text-xl data-[state=active]:bg-sera-jet data-[state=active]:text-sera-periwinkle"
+            >
+              Résumé
+            </TabsTrigger>
+            <TabsTrigger
+              value="ressources"
+              className="rounded-b-none rounded-t-sm text-xl data-[state=active]:bg-sera-jet data-[state=active]:text-sera-periwinkle"
+            >
+              Ressources partagées
+            </TabsTrigger>
+            <TabsTrigger
+              value="members"
+              className="rounded-b-none rounded-t-sm text-xl data-[state=active]:bg-sera-jet data-[state=active]:text-sera-periwinkle"
+            >
+              Membres
+            </TabsTrigger>
+          </TabsList>
+
+          <Separator className="mb-2 h-0.5 w-full bg-sera-jet"></Separator>
+
+          <TabsContent value="resume">
+            <div className="flex justify-start">
+              <h3 className="text-xl font-semibold">Project name :</h3>
+              <p className="mt-auto">{projectData.title}</p>
             </div>
-            <div className="mx-6 mt-10">
-              <div className="flex items-center justify-between">
-                <h3 className="text-4xl">Shared ressources :</h3>
-                <Dialog>
-                  <DialogTrigger>
-                    <Button className="bg-sera-jet text-sera-periwinkle hover:bg-sera-jet/50 hover:text-sera-periwinkle/50">
-                      {" "}
-                      Add ressource
-                    </Button>
-                  </DialogTrigger>
-                  <DialogContent>
-                    <DialogHeader>
-                      <DialogTitle>
-                        <div className="flex items-center">
-                          <BadgeHelp size={44} />
-                          <span className="ml-2"> Add ressource </span>
-                        </div>
-                      </DialogTitle>
-                    </DialogHeader>
-                    <div>
-                      <div className="flex flex-col">
-                        <Label htmlFor="title">Title</Label>
-                        <Input
-                          type="text"
-                          id="title"
-                          className="col-span-3"
-                          
-                        />
-                      </div>
-                      <div className="pt-4">
-                        <Label htmlFor="description">Description</Label>
-                        <Input
-                          type="textarea"
-                          id="description"
-                          className="col-span-3"
-                        />
-                      </div>
-                      <div className="pt-4">
-                        <Label htmlFor="description">Link</Label>
-                        <Input
-                          type="textarea"
-                          id="description"
-                          className="col-span-3"
-                        />
-                      </div>
-                      <div className="pt-4">
-                        <Label htmlFor="description">Tag</Label>
-                        <Input
-                          type="textarea"
-                          id="description"
-                          className="col-span-3"
-                        />
-                      </div>
-                    </div>
-                    <DialogFooter>
-                    <DialogTrigger>
-                      <Button className="bg-sera-jet text-sera-periwinkle hover:bg-sera-jet/50 hover:text-sera-periwinkle/50">
-                        Cancel
-                      </Button>
-                      </DialogTrigger>
-                      <Button type="submit" className="bg-sera-periwinkle text-sera-jet hover:bg-sera-periwinkle/50 hover:text-sera-jet/50">
-                        add ressource
-                      </Button>
-                    </DialogFooter>
-                  </DialogContent>
-                </Dialog>
-              </div>
-              {/* <div className="mt-8 grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-              {project.ressources.map((ressource, index) => (
-                <SharedRessources
-                  key={index}
-                  skeleton={project.skeleton}
-                  name={ressource.name}
-                  description={ressource.description}
-                  date={ressource.Date}
-                  url={ressource.url}
-                />
-              ))}
-            </div> */}
-            </div>
-          </>
-        ) : (
-          <BigLoader
-            loaderSize={42}
-            bgColor="transparent"
-            textColor="sera-jet"
-          />
-        )}
+            <h3 className="text-xl font-semibold">Description :</h3>
+            <p className="text-normal mt-2">{projectData.description}</p>
+          </TabsContent>
+          <TabsContent value="ressources">
+            <SharedContainer />
+          </TabsContent>
+          <TabsContent value="members" className="text-sera-jet">
+            <MembersContainer />
+          </TabsContent>
+        </Tabs>
+
+        <StepsIndicatorContainer />
       </div>
     </>
   );
