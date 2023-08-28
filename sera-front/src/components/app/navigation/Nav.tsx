b9cc401e580a61a078368df749c6240e0acc6539
Antoine Azevedo Da Silva <azevedo-a@hotmail.fr>
2023-08-28 18:17:05 +0200

    feat(rooms-page): Add management page for the rooms
    - Added Modal to edit, create and get the last infos
    - Created routes
    - Updated developement name in env var
    - Updated nav bar to add rooms
    - Added some functions in utils

--- sera-front/src/components/app/navigation/Nav.tsx
+++ sera-front/src/components/app/navigation/Nav.tsx
@@ -1,5 +1,5 @@
 import clsx from "clsx";
-import { BookOpen, Ticket } from "lucide-react";
+import { BookOpen, DoorOpen, Ticket } from "lucide-react";
 import { Link, matchPath, useLocation } from "react-router-dom";
 
 export const Nav = () => {
@@ -8,6 +8,21 @@ export const Nav = () => {
   return (
     <nav className="h-[90vh] w-[14%] border-r-2 border-[#D3D4D5]">
       <ul className="flex h-full flex-col justify-start py-4">
+        <Link
+          to="/dashboard/rooms"
+          className={clsx(
+            "mx-2 my-2 h-14 rounded-lg",
+            "flex justify-around text-sera-jet",
+            matchPath(pathname.pathname, "/dashboard/rooms") &&
+              "bg-sera-jet text-sera-periwinkle",
+            "hover:cursor-pointer hover:bg-sera-jet hover:text-sera-periwinkle"
+          )}
+        >
+          <DoorOpen size={32} className="my-auto " />
+          <p className="my-auto hidden text-2xl font-semibold lg:block">
+            Rooms
+          </p>
+        </Link>
         <Link
           to="/dashboard/tickets"
           className={clsx(
