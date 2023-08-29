6bb10895ef5e47f68c94c808a9bfd54630072f0f
Antoine Azevedo Da Silva <azevedo-a@hotmail.fr>
2023-08-29 19:05:39 +0200

    feat(planification): Add team management
    - Added people search
    - added complete team verification

--- sera-front/src/pages/Planification.tsx
+++ sera-front/src/pages/Planification.tsx
@@ -1,8 +1,10 @@
+import { Check } from "lucide-react";
 import { useState } from "react";
 
 import { HeaderTitle } from "@/components/app/navigation/HeaderTitle";
 import { MembersContainer } from "@/components/app/project/Members/MembersContainer";
-import { RoomContainer } from "@/components/app/project/Rooms/RoomContainer";
+import { ReservationContainer } from "@/components/app/project/Reservation/ReservationContainer";
+import { Button } from "@/components/ui/button";
 
 export const Planification = () => {
   const [teamIsValid, setTeamIsValid] = useState(false);
@@ -11,12 +13,30 @@ export const Planification = () => {
   return (
     <>
       <HeaderTitle title="Planification" previousTitle="Projet" />
-      <div id="planification" className="mx-6 flex justify-between">
-        <section id="planification-team" className="w-5/12">
-          <MembersContainer validTeamSetter={setTeamIsValid} />
+      <div className="mx-6 flex flex-col justify-end">
+        <Button
+          className="bg-sera-jet text-sera-periwinkle hover:bg-sera-jet/50 hover:text-sera-periwinkle/50"
+          disabled={!teamIsValid}
+        >
+          <Check />
+          <p className="ml-2">Validate this step</p>
+        </Button>
+        {!teamIsValid && (
+          <p className="my-auto text-gray-600">
+            You can&apos;t validate this step until your team is complete and
+            you have at least one reservation.
+          </p>
+        )}
+      </div>
+      <div id="planification" className="mx-6 flex flex-col justify-start">
+        <section id="planification-team" className="">
+          <MembersContainer
+            validTeam={teamIsValid}
+            validTeamSetter={setTeamIsValid}
+          />
         </section>
-        <section id="rooms-container" className="w-5/12">
-          <RoomContainer />
+        <section id="rooms-container" className="">
+          <ReservationContainer />
         </section>
       </div>
     </>
