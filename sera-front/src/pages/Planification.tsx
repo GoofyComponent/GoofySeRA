aa6efc86a9410ee7079d5f3952e0046d1f03fa2b
Antoine Azevedo Da Silva <azevedo-a@hotmail.fr>
2023-08-30 14:05:32 +0200

    feat(planification-page): Add reservation method
    - Added modal to reserve
    - Add checks to pass to the next step

--- sera-front/src/pages/Planification.tsx
+++ sera-front/src/pages/Planification.tsx
@@ -1,14 +1,42 @@
+import { useMutation } from "@tanstack/react-query";
 import { Check } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 import { HeaderTitle } from "@/components/app/navigation/HeaderTitle";
 import { MembersContainer } from "@/components/app/project/Members/MembersContainer";
 import { ReservationContainer } from "@/components/app/project/Reservation/ReservationContainer";
 import { Button } from "@/components/ui/button";
+import { axios } from "@/lib/axios";
 
 export const Planification = () => {
+  const { ProjectId } = useParams<{ ProjectId: string }>();
   const [teamIsValid, setTeamIsValid] = useState(false);
-  console.log("teamIsValid", teamIsValid);
+  const [reservationIsValid, setReservationIsValid] = useState(false);
+  const [isPlanificationValid, setIsPlanificationValid] = useState(false);
+
+  const passToCaptation = useMutation({
+    mutationFn: async () => {
+      const moveStep = await axios.post(
+        `/api/projects/${ProjectId}/planification-to-captation`
+      );
+      return moveStep.data;
+    },
+    onSuccess: (response: any) => {
+      console.log("response", response);
+    },
+    onError: (error: any) => {
+      console.log("error", error);
+    },
+  });
+
+  useEffect(() => {
+    if (teamIsValid && reservationIsValid) {
+      setIsPlanificationValid(true);
+    } else {
+      setIsPlanificationValid(false);
+    }
+  }, [teamIsValid, reservationIsValid]);
 
   return (
     <>
@@ -16,12 +44,17 @@ export const Planification = () => {
       <div className="mx-6 flex flex-col justify-end">
         <Button
           className="bg-sera-jet text-sera-periwinkle hover:bg-sera-jet/50 hover:text-sera-periwinkle/50"
-          disabled={!teamIsValid}
+          disabled={!isPlanificationValid}
+          onClick={() => {
+            if (isPlanificationValid) {
+              passToCaptation.mutate();
+            }
+          }}
         >
           <Check />
           <p className="ml-2">Validate this step</p>
         </Button>
-        {!teamIsValid && (
+        {!isPlanificationValid && (
           <p className="my-auto text-gray-600">
             You can&apos;t validate this step until your team is complete and
             you have at least one reservation.
@@ -36,7 +69,10 @@ export const Planification = () => {
           />
         </section>
         <section id="rooms-container" className="">
-          <ReservationContainer />
+          <ReservationContainer
+            validReservation={reservationIsValid}
+            validReservationSetter={setReservationIsValid}
+          />
         </section>
       </div>
     </>
