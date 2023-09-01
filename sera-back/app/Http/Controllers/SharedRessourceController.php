b01f6862159ee0b14c3db7d0f3a23d274d0bd23d
Adrien Albuquerque <adrialb95@gmail.com>
2023-09-01 19:39:30 +0200

    feat(ressource): WIP Store a ressource

--- sera-back/app/Http/Controllers/SharedRessourceController.php
+++ sera-back/app/Http/Controllers/SharedRessourceController.php
@@ -29,6 +29,35 @@ class SharedRessourceController extends Controller
 
     public function store(Request $request)
     {
+        // FRONT -> BACK -> BUCKET -> REPONSE au BACK -> REPONSE au FRONT
+
+        $acceptedTypes = ['image','audio', 'document'];
+
+        $this->validate($request, [
+            'project_id' => 'required|integer',
+            'name' => 'required|string',
+            'type' => 'required|string|in:'.implode(',', $acceptedTypes),
+            'description' => 'required|string',
+            'file' => 'required|file'
+        ]);
+
+        $project = Project::find($request->project_id);
+
+        if (!$project) {
+            return response()->json([
+                'message' => 'Le projet n\'existe pas'
+            ], 400);
+        }
+        $ressource = new Ressource();
+
+        /**
+        * Etape 1: stocker dans le modèle ce qu'on a déjà
+        * Etape 2: Créer le nom du fichier
+        * Etape 3: Stocker le fichier dans le bucket
+        * Etape 4: Stocker dans l'URL de notre ressource dans le modèle
+        */
+
+        return response()->json($ressource, 201);
 
     }
 
