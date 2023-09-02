0a11599b3e0f51e8fd2dd41bba238d902800cc62
lucag322 <97728486+lucag322@users.noreply.github.com>
2023-09-02 21:36:10 +0200

    fix: update recent projects component
    - removal of the "All Project" card
    -  addition of a fourth project card
    - addition of a button to navigate to the 'All Projects' page

--- sera-front/src/components/app/home/RecentProjects.tsx
+++ sera-front/src/components/app/home/RecentProjects.tsx
@@ -1,6 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import { Link } from "react-router-dom";
-
+import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card-project";
 import { axios } from "@/lib/axios";
 import { BigLoader } from "@/pages/skeletons/BigLoader";
@@ -27,7 +27,7 @@ const RecentProjects = () => {
     queryKey: ["recentprojects", { page }],
     queryFn: async () => {
       const recentprojects = await axios.get(
-        `api/projects?page=${page}&sort=desc&maxPerPage=3`
+        `api/projects?page=${page}&sort=desc&maxPerPage=4`
       );
       return recentprojects.data;
     },
@@ -35,16 +35,23 @@ const RecentProjects = () => {
 
   return (
     <div className="m-6 rounded-lg bg-sera-grey-bg px-4 pb-4 pt-2">
-      <h2 className="mb-2 text-4xl font-semibold text-sera-jet">
-        Recent Projects{" "}
-        {!isLoading
-          ? recentprojectsData.data && (
-              <span className="text-sm font-normal italic">
-                This is the last 3 recent projects
-              </span>
-            )
-          : null}
-      </h2>
+      <div className="flex justify-between py-2">
+        <h2 className="mb-2 text-4xl font-semibold text-sera-jet">
+          Recent Projects{" "}
+          {!isLoading
+            ? recentprojectsData.data && (
+                <span className="text-sm font-normal italic">
+                  This is the last 3 recent projects
+                </span>
+              )
+            : null}
+        </h2>
+        <Link to="/dashboard/projects" className="my-auto mr-4">
+          <Button className="bg-sera-jet text-sera-periwinkle hover:bg-sera-jet/50 hover:text-sera-periwinkle/50">
+            View all
+          </Button>
+        </Link>
+      </div>
       {!isLoading ? (
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
           {recentprojectsData.data.map(
@@ -67,14 +74,6 @@ const RecentProjects = () => {
               />
             )
           )}
-          <Link to={"projects"}>
-            <div className="h-[150px] overflow-hidden  text-ellipsis rounded-lg border-2 bg-sera-jet bg-cover bg-center p-3 text-white duration-300 ease-in-out hover:scale-105 ">
-              <div className="flex items-center justify-between text-xl">
-                <span>See more...</span>
-              </div>
-              <p className="pt-8">There is XX another projects</p>
-            </div>
-          </Link>
         </div>
       ) : (
         <div className="flex max-h-[9em] w-full items-center">
