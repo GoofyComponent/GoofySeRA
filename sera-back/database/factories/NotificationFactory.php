6412b12145e2f6f27b1adb487c7675125771d800
Adrien Albuquerque <adrialb95@gmail.com>
2023-09-04 23:14:34 +0200

    feat: Create the migrations for notifications + the seeders

---
+++ sera-back/database/factories/NotificationFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Notification>
+ */
+class NotificationFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            "title" => $this->faker->realText(50),
+            "description" => $this->faker->realText(200),
+            "is_read" => $this->faker->boolean(),
+            "is_deleted" => $this->faker->boolean(),
+            "is_urgent" => $this->faker->boolean(),
+        ];
+    }
+}
