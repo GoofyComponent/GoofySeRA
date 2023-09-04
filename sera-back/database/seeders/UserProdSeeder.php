794b2ede1a94f9b7f542c1a165963f29e795f33a
Adrien Albuquerque <adrialb95@gmail.com>
2023-09-04 15:37:52 +0200

    fix(seeder): fix seeder for prod

---
+++ sera-back/database/seeders/UserProdSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use App\Services\CreateMinioUser;
+use Illuminate\Support\Facades\Crypt;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserProdSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $s3_credentials = (new CreateMinioUser())->create();
+        $s3_credentials['secretkey'] = Crypt::encrypt($s3_credentials['secretkey']);
+        $s3_credentials['accesskey'] = Crypt::encrypt($s3_credentials['accesskey']);
+
+        // on search si l'utilisateur existe déjà
+        if (\App\Models\User::where('email', 'admin@sera.com')->exists()) {
+            return;
+        }
+
+        // adapted for production and use only model and not factory
+        $user = \App\Models\User::firstOrCreate([
+            'email' => 'admin@sera.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+            'firstname' => 'Admin',
+            'lastname' => 'Istrator',
+            'role' => 'cursus_director',
+            'avatar_filename' => 'lulu',
+            's3_credentials' => json_encode($s3_credentials),
+        ]);
+
+    }
+}
