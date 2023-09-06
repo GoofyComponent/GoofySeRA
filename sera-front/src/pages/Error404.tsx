b478fa75308a7b4b472e7cd91ab7000cc4d2f416
lucag322 <97728486+lucag322@users.noreply.github.com>
2023-09-07 00:19:58 +0200

    feeat: add easterEgg

--- sera-front/src/pages/Error404.tsx
+++ sera-front/src/pages/Error404.tsx
@@ -1,13 +1,18 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import Chockbar from "../assets/images/chockbar-404.png";
-import "../assets/styles/chockbar.css"
+import chokbarimg from "../assets/images/chokbar-404.png";
+import Sera from "../assets/images/sera-logo.svg";
+import "../assets/styles/chokbar.css";
+
+import { Dialog, DialogContent } from "@/components/ui/dialog";
+
 export const Error404 = () => {
   //detect when the user press the key "c,h,o,c,k,b,a,r" in this order
-  const [chockbar, setChockbar] = useState("");
+  const [chokbar, setchokbar] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
   useEffect(() => {
     const handleKeyDown = (e: any) => {
-      setChockbar((prev) => prev + e.key);
+      setchokbar((prev) => prev + e.key);
     };
     window.addEventListener("keydown", handleKeyDown);
     return () => {
@@ -15,13 +20,12 @@ export const Error404 = () => {
     };
   }, []);
   useEffect(() => {
-    if (chockbar === "chockbar") {
-      window.location.href = "https://www.youtube.com/watch?v=V5BruGgWfS4";
+    if (chokbar === "chokbar") {
+      setIsOpen(true);
     }
-  }, [chockbar]);
+  }, [chokbar]);
 
-
-  const letters = ["C", "H", "O", "C", "K", "B", "A", "R"]
+  const letters = ["C", "H", "O", "C", "B", "A", "R"];
   const [currentLetterIndex, setCurrentLetterIndex] = useState(0);
 
   useEffect(() => {
@@ -32,27 +36,43 @@ export const Error404 = () => {
     return () => clearInterval(interval);
   }, [letters.length]);
 
+  function handleDilogOpen() {
+    setIsOpen((prev) => !prev);
+    setchokbar("");
+  }
 
   return (
     <>
-      <div className="flex flex-col items-center w-screen justify-center h-screen">
-        <img src={Chockbar} alt="chockbar" className="w-1/5" />
-        <h2 className="text-4xl font-semibold text-sera-periwinkle italic text-center">
-          404 - Chockbar not found
+      <div className="relative flex h-screen w-screen flex-col items-center justify-center">
+        <img
+          src={Sera}
+          alt="sera-logo"
+          className="img-bg absolute top-1/2 my-auto w-full translate-y-[-50%] opacity-5"
+        />
+        <img src={chokbarimg} alt="chokbar" className="w-1/5" />
+        <h2 className="text-center text-4xl font-semibold italic text-sera-periwinkle">
+          404 - chokbar not found
         </h2>
         <Link to={"/dashboard"}>
-          <button className="mt-4 bg-sera-periwinkle font-bold py-2 px-4 rounded text-sera-jet ease-in duration-100 hover:scale-105 ">
+          <button className="mt-4 rounded bg-sera-periwinkle px-4 py-2 font-bold text-sera-jet duration-100 ease-in hover:scale-105 ">
             Go back to dashboard
           </button>
         </Link>
         {letters.map((letter, index) => (
-        <span
-          key={index}
-          className={`letter ${index === currentLetterIndex ? 'visible' : ''}`}
-        >
-          {letter}
-        </span>
-      ))}
+          <span
+            key={index}
+            className={`letter ${
+              index === currentLetterIndex ? "visible" : ""
+            }`}
+          >
+            {letter}
+          </span>
+        ))}
+        <Dialog onOpenChange={handleDilogOpen} open={isOpen}>
+          <DialogContent>
+            <p>LA VIDEO GNGNGN</p>
+          </DialogContent>
+        </Dialog>
       </div>
     </>
   );
