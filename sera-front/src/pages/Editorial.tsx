d7dca10bd77efaab4ae8343312ca339223bff39f
lucalg322 <l_grousset@hetic.eu>
2023-09-06 15:01:28 +0200

    refactor: editorial page

--- sera-front/src/pages/Editorial.tsx
+++ sera-front/src/pages/Editorial.tsx
@@ -1,6 +1,5 @@
 import { HeaderTitle } from "@/components/app/navigation/HeaderTitle";
 import { EditorialForm } from "@/components/app/editorial/EditorialForm";
-import { BiofraohieForm } from "@/components/app/editorial/BiofraohieForm";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
@@ -16,6 +15,7 @@ import {
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { Search } from "lucide-react";
 
 export const Editorial = () => {
   const images = [
@@ -24,7 +24,12 @@ export const Editorial = () => {
     { nom: "image3", url: "https://source.unsplash.com/random/700x800" },
     { nom: "image4", url: "https://source.unsplash.com/random/500x500" },
     { nom: "image5", url: "https://source.unsplash.com/random/600x400" },
-    { nom: "image6", url: "https://source.unsplash.com/random/300x900" },
+    { nom: "image7", url: "https://source.unsplash.com/random/200x200" },
+    { nom: "image1", url: "https://source.unsplash.com/random/800x500" },
+    { nom: "image2", url: "https://source.unsplash.com/random/600x400" },
+    { nom: "image3", url: "https://source.unsplash.com/random/700x800" },
+    { nom: "image4", url: "https://source.unsplash.com/random/500x500" },
+    { nom: "image5", url: "https://source.unsplash.com/random/600x400" },
     { nom: "image7", url: "https://source.unsplash.com/random/200x200" },
   ];
 
@@ -32,7 +37,7 @@ export const Editorial = () => {
     dots: true,
     infinite: false,
     speed: 500,
-    slidesToShow: 3,
+    slidesToShow: 4,
     slidesToScroll: 2,
     arrows: false,
     nextArrow: <></>,
@@ -50,6 +55,7 @@ export const Editorial = () => {
           <div className="h-full w-full rounded border "></div>
         </div>
       </div>
+
       <div className="flex w-full flex-row">
         <div className="ml-6 mr-3 flex w-1/2 flex-col justify-end space-y-2">
           <div className="py-2">
@@ -68,60 +74,68 @@ export const Editorial = () => {
             </div>
           </div>
         </div>
-        <div className="ml-3 mr-6 flex w-1/2 flex-col justify-end space-y-2"></div>
-      </div>
-      <div className="mt-2 flex w-full flex-row">
-        <div className="ml-6 mr-3 flex w-1/2 flex-col justify-end ">
-          <Slider {...settings}>
-            {images.map((image: any) => {
-              return (
+        <div className="ml-3 mr-6 flex w-1/2 flex-col justify-end space-y-2">
+          <div className="mt-2 flex w-full flex-row">
+            <div className="ml-6 mr-3 flex w-1/2 flex-col justify-end space-y-2">
+              <div className="flex flex-col items-baseline py-2">
+                <Label className="mr-2 align-middle text-lg">Knowladge</Label>
                 <Dialog>
                   <DialogTrigger>
-                    <img
-                      className="aspect-square rounded object-cover p-1"
-                      src={image.url}
-                      alt={image.nom}
-                    />
+                    <div>
+                      <Button className="mt-0 bg-sera-jet text-sera-periwinkle hover:bg-sera-jet/50 hover:text-sera-periwinkle/50">
+                        Add Knowladge
+                      </Button>
+                    </div>
                   </DialogTrigger>
                   <DialogContent>
                     <DialogHeader>
-                      <DialogTitle>{image.nom}</DialogTitle>
+                      <DialogTitle>Add Knowladge</DialogTitle>
                       <DialogDescription>
-                        <img className="" src={image.url} alt={image.nom} />
+                        <div className="flex">
+                          <Input
+                            className="mr-1 w-4/5 border border-sera-jet"
+                            type="text"
+                            placeholder="Search knowladge"
+                          />
+                          <Button className="w-1/5 bg-sera-jet text-sera-periwinkle hover:bg-sera-jet/50 hover:text-sera-periwinkle/50">
+                            <Search size={24} />
+                          </Button>
+                        </div>
                       </DialogDescription>
                     </DialogHeader>
                   </DialogContent>
                 </Dialog>
-              );
-            })}
-          </Slider>
-        </div>
-        <div className="ml-3 mr-6 flex w-1/2 flex-col justify-end space-y-2"></div>
-      </div>
-      <div className="mt-2 flex w-full flex-row">
-        <div className="ml-6 mr-3 flex w-1/2 flex-col justify-end space-y-2">
-          <div className="flex py-2">
-            <Label className="mr-2 align-middle text-lg">Biographie</Label>
-            <Dialog>
-              <DialogTrigger>
-                <Button className="mt-0 bg-sera-jet text-sera-periwinkle hover:bg-sera-jet/50 hover:text-sera-periwinkle/50">
-                  Add biography
-                </Button>
-              </DialogTrigger>
-              <DialogContent>
-                <DialogHeader>
-                  <DialogTitle>Add biography</DialogTitle>
-                  <DialogDescription>
-                    <BiofraohieForm />
-                  </DialogDescription>
-                </DialogHeader>
-              </DialogContent>
-            </Dialog>
+              </div>
+            </div>
           </div>
         </div>
-        <div className="ml-3 mr-6 flex w-1/2 flex-col justify-end space-y-2">
-          <div className="py-2">
-            <Label className="text-lg">Réseaux sociaux</Label>
+      </div>
+      <div className="mt-2 flex w-full flex-row">
+        <div className=" flex w-full flex-col justify-end ">
+          <div className="mx-4">
+            <Slider {...settings}>
+              {images.map((image: any) => {
+                return (
+                  <Dialog>
+                    <DialogTrigger>
+                      <img
+                        className="w-100 aspect-square rounded object-cover p-1"
+                        src={image.url}
+                        alt={image.nom}
+                      />
+                    </DialogTrigger>
+                    <DialogContent>
+                      <DialogHeader>
+                        <DialogTitle>{image.nom}</DialogTitle>
+                        <DialogDescription>
+                          <img className="" src={image.url} alt={image.nom} />
+                        </DialogDescription>
+                      </DialogHeader>
+                    </DialogContent>
+                  </Dialog>
+                );
+              })}
+            </Slider>
           </div>
         </div>
       </div>
