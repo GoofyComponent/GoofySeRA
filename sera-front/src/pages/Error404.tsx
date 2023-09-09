c4c1510f42fa7efaea376229d0ab8346b024f616
lucag322 <97728486+lucag322@users.noreply.github.com>
2023-09-09 04:01:33 +0200

    fix: change video 404

--- sera-front/src/pages/Error404.tsx
+++ sera-front/src/pages/Error404.tsx
@@ -84,16 +84,7 @@ export const Error404 = () => {
         <Dialog onOpenChange={handleDilogOpen} open={isOpen}>
           <DialogContent>
             <div className="p-2">
-              <iframe
-                className="ratio-16/9"
-                width="100%"
-                height="100%"
-                src="https://www.youtube-nocookie.com/embed/l_Lb0UvXmWk?si=j8RGkGAqzKwdQpeS&autoplay=1&amp;controls=0"
-                title="YouTube video player"
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                allowFullScreen
-              ></iframe>
+              <iframe width="100%" height="100%" src="https://www.youtube-nocookie.com/embed/zPSjX2YiHto?si=Us9Rjd19M3iGLnV7&autoplay=1&amp;controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             </div>
           </DialogContent>
         </Dialog>
