70fc60189e751a7d1077f169c3c0dab9151b9371
Adrien Albuquerque <adrialb95@gmail.com>
2023-09-12 01:58:39 +0200

    feat(Subtitle): Get and Delete a Subtitle for a lang and a Project

--- sera-back/app/Http/Controllers/SubtitleController.php
+++ sera-back/app/Http/Controllers/SubtitleController.php
@@ -281,4 +281,283 @@ class SubtitleController extends Controller
         ], 200);
     }
 
+    /**
+    * @OA\Get(
+    *     path="/api/projects/{projectId}/subtitles",
+    *     summary="Get subtitles",
+    *     tags={"Subtitles"},
+    *     description="Get subtitles",
+    *     operationId="getSubtitles",
+    *     @OA\Parameter(
+    *         name="projectId",
+    *         in="path",
+    *         description="Id of the project",
+    *         required=true,
+    *         @OA\Schema(
+    *             type="integer",
+    *             format="int64"
+    *         )
+    *     ),
+    *     @OA\Parameter(
+    *         name="lang",
+    *         in="query",
+    *         description="Language of the subtitle",
+    *         required=false,
+    *         @OA\Schema(
+    *             type="string",
+    *         )
+    *     ),
+    *     @OA\Response(
+    *         response=200,
+    *         description="Subtitles successfully retrieved",
+    *         @OA\JsonContent(
+    *             @OA\Property(
+    *                 property="subtitles",
+    *                 type="array",
+    *                 @OA\Items(
+    *                     @OA\Property(
+    *                         property="lang",
+    *                         type="string",
+    *                         example="fr"
+    *                     ),
+    *                     @OA\Property(
+    *                         property="srt",
+    *                         type="object",
+    *                         @OA\Property(
+    *                             property="id",
+    *                             type="integer",
+    *                             example=1
+    *                         ),
+    *                         @OA\Property(
+    *                             property="lang",
+    *                             type="string",
+    *                             example="fr"
+    *                         ),
+    *                         @OA\Property(
+    *                             property="file_type",
+    *                             type="string",
+    *                             example="srt"
+    *                         ),
+    *                         @OA\Property(
+    *                             property="ressource_id",
+    *                             type="integer",
+    *                             example=1
+    *                         ),
+    *                         @OA\Property(
+    *                             property="project_id",
+    *                             type="integer",
+    *                             example=1
+    *                         ),
+    *                         @OA\Property(
+    *                             property="created_at",
+    *                             type="string",
+    *                             example="2021-05-04T09:00:00.000000Z"
+    *                         ),
+    *                         @OA\Property(
+    *                             property="updated_at",
+    *                             type="string",
+    *                             example="2021-05-04T09:00:00.000000Z"
+    *                         ),
+
+    *                     ),
+    *                  ),
+    *             ),
+    *         )
+    *     ),
+    *     @OA\Response(
+    *         response=400,
+    *         description="Bad request",
+    *         @OA\JsonContent(
+    *             @OA\Property(
+    *                 property="message",
+    *                 type="string",
+    *                 example="The given data was invalid."
+    *             ),
+    *             @OA\Property(
+    *                 property="errors",
+    *                 type="object",
+    *                 @OA\Property(
+    *                     property="lang",
+    *                     type="array",
+    *                     @OA\Items(
+    *                         type="string",
+    *                         example="The lang field is required."
+    *                     ),
+    *                 ),
+    *             ),
+    *         )
+    *     ),
+    *     @OA\Response(
+    *         response=404,
+    *         description="Project not found",
+    *         @OA\JsonContent(
+    *             @OA\Property(
+    *                 property="message",
+    *                 type="string",
+    *                 example="Project not found"
+    *             ),
+    *         )
+    *     ),
+    * )
+    */
+    public function index(Request $request, $projectId){
+        $countriesKeys = Countries::getList('en', 'php');
+        $countriesKeys = array_keys($countriesKeys);
+
+        $validator = Validator::make($request->all(), [
+            'lang' => 'string|in:'.implode(',', $countriesKeys),
+        ]);
+
+        $validator->setCustomMessages([
+            'lang.in' => 'Le tableau complet des langues est : '.implode(', ', $countriesKeys),
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => $validator->errors()->first()
+            ], 400);
+        }
+
+        $project = Project::find($projectId);
+        if (!$project) {
+            return response()->json([
+                'message' => 'Project not found'
+            ], 404);
+        }
+
+        $subtitles = Subtitle::where('project_id', $projectId);
+
+        if ($request->has('lang')) {
+            $subtitles = $subtitles->where('lang', $request->lang);
+        }
+
+        $subtitles = $subtitles->with('ressource')->get();
+
+        // on les tries par version dans le truc qu'on renvoie
+        $transformedSubtitles = [];
+        foreach ($subtitles as $subtitle) {
+            $transformedSubtitles[$subtitle->lang][$subtitle->file_type] = $subtitle;
+        }
+
+        return response()->json([
+            'subtitles' => $transformedSubtitles,
+        ], 200);
+    }
+
+
+    /**
+    * @OA\Delete(
+    *     path="/api/projects/{projectId}/subtitles",
+    *     summary="Delete subtitles",
+    *     tags={"Subtitles"},
+    *     description="Delete subtitles",
+    *     operationId="deleteSubtitles",
+    *     @OA\Parameter(
+    *         name="projectId",
+    *         in="path",
+    *         description="Id of the project",
+    *         required=true,
+    *         @OA\Schema(
+    *             type="integer",
+    *             format="int64"
+    *         )
+    *     ),
+    *     @OA\Parameter(
+    *         name="lang",
+    *         in="query",
+    *         description="Language of the subtitle",
+    *         required=true,
+    *         @OA\Schema(
+    *             type="string",
+    *         )
+    *     ),
+    *     @OA\Response(
+    *         response=200,
+    *         description="Subtitles successfully deleted",
+    *         @OA\JsonContent(
+    *             @OA\Property(
+    *                 property="message",
+    *                 type="string",
+    *                 example="Subtitles successfully deleted"
+    *             )
+    *         )
+    *     ),
+    *     @OA\Response(
+    *         response=400,
+    *         description="Bad request",
+    *         @OA\JsonContent(
+    *             @OA\Property(
+    *                 property="message",
+    *                 type="string",
+    *                 example="The given data was invalid."
+    *             ),
+    *             @OA\Property(
+    *                 property="errors",
+    *                 type="object",
+    *                 @OA\Property(
+    *                     property="lang",
+    *                     type="array",
+    *                     @OA\Items(
+    *                         type="string",
+    *                         example="The lang field is required."
+    *                     ),
+    *                 ),
+    *             ),
+    *         )
+    *     ),
+    *     @OA\Response(
+    *         response=404,
+    *         description="Project not found",
+    *         @OA\JsonContent(
+    *             @OA\Property(
+    *                 property="message",
+    *                 type="string",
+    *                 example="Project not found"
+    *             ),
+    *         )
+    *     ),
+    * )
+    */
+    public function destroy(Request $request, $projectId){
+        $countriesKeys = Countries::getList('en', 'php');
+        $countriesKeys = array_keys($countriesKeys);
+
+        $validator = Validator::make($request->all(), [
+            'lang' => 'required|string|in:'.implode(',', $countriesKeys),
+        ]);
+
+        $validator->setCustomMessages([
+            'lang.in' => 'Le tableau complet des langues est : '.implode(', ', $countriesKeys),
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => $validator->errors()->first()
+            ], 400);
+        }
+
+        $project = Project::find($projectId);
+        if (!$project) {
+            return response()->json([
+                'message' => 'Project not found'
+            ], 404);
+        }
+
+        // on récupère les sous-titres du projet par langue
+        $subtitles = Subtitle::where('project_id', $projectId)->where('lang', $request->lang)->get();
+        if ($subtitles->isEmpty()) {
+            return response()->json([
+                'message' => 'There is no subtitles for this language'
+            ], 404);
+        }
+
+        // on supprime les ressources associées
+        foreach ($subtitles as $subtitle) {
+            $subtitle->ressource->delete();
+        }
+
+        return response()->json([
+            'message' => 'Subtitles successfully deleted',
+        ], 200);
+    }
 }
