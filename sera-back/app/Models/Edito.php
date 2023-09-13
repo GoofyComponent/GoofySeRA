5db483f240bdbac25c94e9921b0e7c965bbd8139
Adrien Albuquerque <adrialb95@gmail.com>
2023-09-14 00:19:51 +0200

    fix: Minio url for Edito

--- sera-back/app/Models/Edito.php
+++ sera-back/app/Models/Edito.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Edito extends Model
 {
@@ -14,8 +15,32 @@ class Edito extends Model
         return $this->hasOne(Project::class);
     }
 
-    public function knowledges()
+    public function getimagesAttribute($value)
     {
-        return $this->belongsToMany(Knowledge::class, 'edito_knowledge', 'edito_id', 'knowledge_id');
+        // si value null, return null
+        if (!$value) {
+            return null;
+        }
+
+        if (env('IS_LOCAL')) {
+            $config = config('filesystems.disks.s3');
+            $config['url'] = 'http://localhost:9000';
+            $config['endpoint'] = 'http://localhost:9000';
+            config(['filesystems.disks.s3' => $config]);
+        }
+
+        $disk = Storage::disk('s3');
+
+        $value =  array_values((array) json_decode($value));
+        $temporaryUrl = [];
+
+        foreach ($value as $key => $image) {
+            $temporaryUrl[$key] = $disk->temporaryUrl(
+                $image,
+                now()->addHours(24)
+            );
+        }
+
+        return json_encode($temporaryUrl);
     }
 }
