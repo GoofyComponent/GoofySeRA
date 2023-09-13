776e2194f08083d7f5a3919e5fb36fa920aa376d
Adrien Albuquerque <adrialb95@gmail.com>
2023-09-13 18:49:18 +0200

    fix(Seeder): Fix seeder for project 6

--- sera-back/database/seeders/ProjectSeeder.php
+++ sera-back/database/seeders/ProjectSeeder.php
@@ -139,7 +139,7 @@ class ProjectSeeder extends Seeder
         $videowReview->type = 'video/mp4';
         $videowReview->resolution = '720';
         $videowReview->provider = 'html5';
-        $videowReview->validated = false;
+        $videowReview->validated = true;
         $videowReview->save();
 
         $videowComment = new \App\Models\CommentReview();
