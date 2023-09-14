20b3846ac303db6e05bacfa79f5601d263749bd4
lucag322 <97728486+lucag322@users.noreply.github.com>
2023-09-15 01:25:46 +0200

    feat: add editorial and add update editorial

---
+++ sera-front/src/components/app/edito/Addknowledge.tsx
@@ -0,0 +1,52 @@
+import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { axios } from "@/lib/axios";
+import { Addknowledgetable } from "./AddknowldegeTable";
+
+export const Addknowledge = () => {
+  const [searchInput, setSearchInput] = useState("");
+
+  const { data: knowledgeData, refetch: refetchKnowledge } = useQuery({
+    queryKey: ["knowledge"],
+    queryFn: async () => {
+      let call = "/api/knowledges";
+      if (searchInput.trim() !== "") {
+        call = `/api/knowledges?search=${searchInput}`;
+      }
+      const knowledge = await axios.get(call);
+      return knowledge.data;
+    },
+  });
+  return (
+    <>
+      <div className="flex flex-col items-center">
+        <div className="flex justify-start">
+          <Input
+            className="mr-2 w-[360px]"
+            type="text"
+            placeholder="Search in the database"
+            value={searchInput}
+            onChange={(e) => {
+              const inputValue = e.target.value;
+              setSearchInput(inputValue);
+              if (inputValue.trim() === "") {
+                refetchKnowledge();
+              }
+            }}
+          />
+          <Button
+            onClick={() => refetchKnowledge()}
+            className="bg-sera-jet text-sera-periwinkle hover:bg-sera-jet/50 hover:text-sera-periwinkle/50"
+          >
+            Search
+          </Button>
+        </div>
+        <div className="h-[300px] overflow-y-scroll">
+          <Addknowledgetable datas={knowledgeData} />
+        </div>
+      </div>
+    </>
+  );
+};
