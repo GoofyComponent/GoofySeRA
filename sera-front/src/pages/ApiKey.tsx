d0502b471f335a4efae6688b0fb8c0129c5a7c07
Antoine Azevedo Da Silva <azevedo-a@hotmail.fr>
2023-09-14 22:42:52 +0200

    feat: update logic to open renew & delete modal

--- sera-front/src/pages/ApiKey.tsx
+++ sera-front/src/pages/ApiKey.tsx
@@ -1,15 +1,27 @@
 import { useEffect, useState } from "react";
-import { useNavigate, useSearchParams } from "react-router-dom";
+import {
+  useLocation,
+  useNavigate,
+  useParams,
+  useSearchParams,
+} from "react-router-dom";
 
-import { AddKeyDialog } from "@/components/app/apiKey/ApiKeyModal";
+import {
+  AddKeyDialog,
+  DeleteKeyDialog,
+  RenewKeyDialog,
+} from "@/components/app/apiKey/ApiKeyModal";
 import { ApiKeyTable } from "@/components/app/apiKey/ApiKeyTable";
 import { Button } from "@/components/ui/button";
 import { Dialog } from "@/components/ui/dialog";
 import { accessManager } from "@/lib/utils";
 
 export const ApiKey = () => {
+  const location = useLocation();
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
+  const { keyId } = useParams<{ keyId: string }>();
+
   const [addApiKeyDialog, setAddApiKeyDialog] = useState(false);
 
   useEffect(() => {
@@ -17,11 +29,13 @@ export const ApiKey = () => {
       return navigate("/dashboard");
     }
 
-    if (searchParams.get("action") || addApiKeyDialog) {
-      navigate("/dashboard/api?action=add");
+    console.log(searchParams.get("action"));
+
+    if (searchParams.get("action") && !addApiKeyDialog) {
+      console.log("Inside openModal");
       setAddApiKeyDialog(true);
     }
-  }, [addApiKeyDialog]);
+  }, [location]);
 
   return (
     <section className="flex h-full flex-col">
@@ -30,7 +44,7 @@ export const ApiKey = () => {
         <div className="flex justify-between">
           {accessManager(undefined, "add_api_key") && (
             <Button
-              onClick={() => setAddApiKeyDialog(true)}
+              onClick={() => navigate("/dashboard/api?action=add")}
               className="bg-sera-jet text-sera-periwinkle hover:bg-sera-jet/50 hover:text-sera-periwinkle/50"
             >
               Create a new key
@@ -48,7 +62,11 @@ export const ApiKey = () => {
         }}
         open={addApiKeyDialog}
       >
-        <AddKeyDialog />
+        {searchParams.get("action") === "add" && <AddKeyDialog />}
+        {searchParams.get("action") === "renew" && keyId && <RenewKeyDialog />}
+        {searchParams.get("action") === "delete" && keyId && (
+          <DeleteKeyDialog />
+        )}
       </Dialog>
     </section>
   );
